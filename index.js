d8cc9cd341ce86308bd3150260650b4bfca48e48
Bryan Parada <bryan.parada.cea@gmail.com>
2023-02-06 16:38:03 -0300

    FA20 - Socket.io - initial config

--- index.js
+++ index.js
@@ -2,14 +2,30 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 
+//App de express
 const app = express();
+
+//Node server
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+//mensajes de sockets
+io.on('connection', client => { 
+    console.log('Client connected');
+
+    client.on('disconnect', () => {
+        console.log('Client disconnected');
+        
+     });
+  }); 
+
 
 //path publico
 const publicPath = path.resolve( __dirname, 'public' );
 
 app.use( express.static(publicPath) )
 
-app.listen(process.env.PORT, (err) =>{
+server.listen(process.env.PORT, (err) =>{
 
     if ( err ) throw new Error(err);
 
