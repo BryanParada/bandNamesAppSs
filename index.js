51d73b3af300615cf8abcd52f64f00e9f15389f8
Bryan Parada <bryan.parada.cea@gmail.com>
2023-02-06 15:50:27 -0300

    FA19 - Env variables - scripts

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+require('dotenv').config();
 
+const app = express();
 
 //path publico
 const publicPath = path.resolve( __dirname, 'public' );
 
 app.use( express.static(publicPath) )
 
-app.listen(3000, (err) =>{
+app.listen(process.env.PORT, (err) =>{
 
     if ( err ) throw new Error(err);
 
-    console.log(`Server running on port`, 3000);
+    console.log(`Server running on port`, process.env.PORT);
 
 });
